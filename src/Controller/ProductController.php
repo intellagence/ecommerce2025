1911ade6f5afddddea1bf75ca9d112eedf903bae
bart <bldlr170289@gmail.com>
2025-07-04 17:04:31 +0200

    crud product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductForm;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,14 +14,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class ProductController extends AbstractController
 {
     #[Route('/produit/afficher', name:'app_product_index')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-       return $this->render('product/index.html.twig');
+        /*
+            Lorsqu'une entity est créée, son repository est généré également
+            entity = table
+            repository = requête SELECT
+        */
+
+        $products = $productRepository->findAll();
+
+        //dd($products);
+
+        /*
+            Dans tous les repository, il existe 4 méthodes par défaut :
+            - findAll() : return []
+            - findBy() : return []
+            - find() : return Objet Product or NULL
+            - findOneBy() : return Objet Product or NULL
+
+            ProductRepository : SELECT ........ FROM product ......
+            1- findAll() : SELECT * FROM product
+
+
+            3- find(int $id) : SELECT * FROM product WHERE id = $id
+        */
+       return $this->render('product/index.html.twig', [
+        'products' => $products
+       ]);
     }
 
     #[Route('/produit/ajouter', name:'app_product_new')]
-    public function new(): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        /*
+            La méthode new() dépend de 2 objets, sans ces 2 derniers la route pour ajouter un produit ne peut pas fonctionner,
+            on définit les dépendances dans les parenthèses de la méthode
+
+            syntaxe : Class $objet, Class2 $objet2
+
+        */
         // Création d'une instance/objet de la class (Entity) Product
         $product = new Product();
         dump($product);
@@ -32,6 +67,40 @@ final class ProductController extends AbstractController
             Cette méthode retourne un objet issu de la class FormInterface
         */
         $form = $this->createForm(ProductForm::class, $product);
+
+        // Traitement du formulaire
+        $form->handleRequest($request);
+
+        dump($form);
+        /*
+            Si le formulaire a été soumis et validé (constraints)
+        */
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($product);
+
+            // EntityManagerInterface est la class qui permet d'exécuter les 3 requètes :
+            // INSERT INTO
+            // UPDATE
+            // DELETE
+
+
+            // INSERTION EN BASE DE DONNÉES
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            //dd($product);
+
+            // notification
+
+
+            // redirection
+            /*
+                La méthode redirectToRoute() permet de faire des redirections internes
+                1e argument (obligatoire) - type : string ==> Nom de la route (attribut name) 
+                2e argument (facultatif)  - type : array  ==> tableau des paramètres
+            */
+            return $this->redirectToRoute('app_product_index');
+        }
 
 
         return $this->render('product/new.html.twig', [
